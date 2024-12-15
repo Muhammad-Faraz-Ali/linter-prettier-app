96c72b394ce057c934c889274f9aac9832b01e71
Muhammad Faraz Ali <muhammad.faraz@confiz.com>
2024-12-15 13:04:15 +0500

    Test commit

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,31 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import prettier from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
     rules: {
       semi: ["error", "always"],
-      quotes: ["error", "double"]
-    }
-  }
-  
+      quotes: ["error", "double"],
+      "no-unused-vars": "error",
+      "no-undef": "error",
+    },
+    linterOptions: {
+      fix: true, // Enable auto-fixing
+    },
+  },
+  prettier,
+  {
+    rules: {
+      "prettier/prettier": ["error"],
+    },
+  },
 ];
